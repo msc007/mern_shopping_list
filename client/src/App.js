7418f0081c71d74f53144f5f6b70ffce5322727d
Bahaness <tl492@hotmail.com>
2018-08-30 16:34:02 -0700

    React Component Navbar with reactstrap added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import AppNavbar from "./components/AppNavbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <AppNavbar />
       </div>
     );
   }
