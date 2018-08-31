150ad600627da984bfd8e48d490806dd06e0a3f0
Bahaness <tl492@hotmail.com>
2018-08-30 17:39:55 -0700

    shoppinglist component added & fade effect added to css

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppNavbar />
+        <ShoppingList />
       </div>
     );
   }
