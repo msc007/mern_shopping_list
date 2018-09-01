5a2c5fb132415925d3589937ced18406fd366511
Bahaness <tl492@hotmail.com>
2018-08-31 19:04:38 -0700

    Initial redux implemented w/o actual database

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import AppNavbar from "./components/AppNavbar";
 import ShoppingList from "./components/ShoppingList";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <ShoppingList />
+        </div>
+      </Provider>
     );
   }
 }
